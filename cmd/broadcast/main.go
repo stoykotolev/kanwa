134b61cc69de8516fefd5783820aa397292d6ada
Stoyko Tolev <st.tolev@gmail.com>
2026-05-24 00:22:33 +0300

    feat(broadcast): Finish challenge 3b. Make it multi-node

--- cmd/broadcast/main.go
+++ cmd/broadcast/main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 type BroadcastMessage struct {
-	Type    string  `json:"type"`
-	Message float64 `json:"message"`
+	Type      string  `json:"type"`
+	MessageId *int    `json:"msg_id"`
+	Message   float64 `json:"message"`
 }
+
 type BroadcastResponse struct {
 	Type string `json:"type"`
+}
+
+type BroadcastNeighbour struct {
+	Type     string    `json:"type"`
+	Messages []float64 `json:"messages"`
 }
 
 type TopologyMessage struct {
@@ -29,10 +37,30 @@ type ReadResponse struct {
 	Messages []float64 `json:"messages"`
 }
 
+var (
+	mu       sync.RWMutex
+	messages = make(map[float64]struct{})
+)
+
+// Add
+func add(msg float64) {
+	mu.Lock()
+	defer mu.Unlock()
+	messages[msg] = struct{}{}
+}
+
+// Check
+func has(msg float64) bool {
+	mu.RLock()
+	defer mu.RUnlock()
+	_, ok := messages[msg]
+	return ok
+}
+
 func main() {
 	n := maelstrom.NewNode()
 
-	// topology := map[string][]string{}
+	neighbours := []string{}
 	n.Handle("topology", func(msg maelstrom.Message) error {
 
 		var body TopologyMessage
@@ -40,12 +68,13 @@ func main() {
 			return err
 		}
 
+		neighbours = body.Topology[n.ID()]
+
 		var responseBody = TopologyResponse{Type: "topology_ok"}
 
 		return n.Reply(msg, responseBody)
 	})
 
-	values := []float64{}
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 
 		var body BroadcastMessage
@@ -53,16 +82,36 @@ func main() {
 			return err
 		}
 
-		values = append(values, body.Message)
+		seen := has(body.Message)
 
-		var response = BroadcastResponse{
-			Type: "broadcast_ok",
+		if !seen {
+			add(body.Message)
+			for _, nh := range neighbours {
+				if err := n.Send(nh, BroadcastMessage{
+					Type:    "broadcast",
+					Message: body.Message,
+				}); err != nil {
+					return err
+				}
+			}
 		}
 
-		return n.Reply(msg, response)
+		if body.MessageId != nil {
+			return n.Reply(msg, BroadcastResponse{
+				Type: "broadcast_ok",
+			})
+		}
+
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
+		mu.RLock()
+		values := make([]float64, 0, len(messages))
+		for k := range messages {
+			values = append(values, k)
+		}
+		mu.RUnlock()
 
 		var body = ReadResponse{
 			Type:     "read_ok",
