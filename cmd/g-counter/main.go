cc6d797be5c4b0029bad13134e72bf63119e95e1
Stoyko Tolev <st.tolev@gmail.com>
2026-08-05 19:19:55 +0300

    feat: Update structs, start returning values. add kv store

--- cmd/g-counter/main.go
+++ cmd/g-counter/main.go
@@ -1,31 +1,62 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type Response struct {
+type AddBody struct {
+	Type  string `json:"type"`
+	Delta int    `json:"delta"`
+}
+type AddResponse struct {
 	Type string `json:"type"`
+}
+
+type ReadResponse struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
 }
 
 func main() {
 	n := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(n)
+	ctx, cancel := context.WithCancel(context.Background())
 
 	n.Handle("add", func(msg maelstrom.Message) error {
-		return n.Reply(msg, &Response{
+		var body AddBody
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			log.Printf("Failed unmarshalling add body. %s", err.Error())
+		}
+		nv := body.Delta
+		v, err := kv.ReadInt(ctx, "value")
+		if err != nil {
+			log.Printf("Failed getting value from kv store. %s", err.Error())
+		}
+		if err := kv.Write(ctx, "value", nv+v); err != nil {
+			log.Printf("Failed setting value in kv store. %s", err.Error())
+		}
+		return n.Reply(msg, AddResponse{
 			Type: "add_ok",
 		})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		return n.Reply(msg, &Response{
-			Type: "add_ok",
+		v, err := kv.ReadInt(ctx, "value")
+		if err != nil {
+			log.Printf("Failed getting value from kv store. %s", err.Error())
+		}
+		return n.Reply(msg, ReadResponse{
+			Type:  "read_ok",
+			Value: v,
 		})
 	})
 
 	if err := n.Run(); err != nil {
+		cancel()
 		log.Fatal(err)
 	}
 }
